4a98a101bc2eb24a8b51be380daa7fd70470b019
ilijastojkovic <ilijastojkovic@yahoo.com>
2020-09-18 16:07:08 +0200

    Fix in configuration file

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -35,7 +35,12 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('jms_job_queue');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root('jms_job_queue');
+        }
 
         $rootNode
             ->children()
