d1002d755322ab53217cf55ee68b3b9943f68362
Evert Jan Hakvoort <evertjan@skrepr.com>
2021-02-16 09:06:11 +0100

    updated managerRegistry

--- Command/CleanUpCommand.php
+++ Command/CleanUpCommand.php
@@ -2,7 +2,7 @@
 
 namespace JMS\JobQueueBundle\Command;
 
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManager;
 use JMS\JobQueueBundle\Entity\Job;
